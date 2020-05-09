2a5cf9e3236b6b6b4890d2577dfe390bc409ff53
Maher <the-maher@live.com>
2020-05-09 22:25:13 +0300

    Use MD in Goback app

--- src/goback/App.vue
+++ src/goback/App.vue
@@ -1,8 +1,12 @@
 <template>
-    <div class="center">
-        <h3>{{randomImage.text}}</h3>
-        <img :src="randomImage.path" width="480">
-    </div>
+        <md-card>
+            <md-card-header v-if="randomImage.text != ''">
+                <div class="md-title">{{randomImage.text}}</div>
+            </md-card-header>
+            <md-card-media>
+                <img :src="randomImage.path" >
+            </md-card-media>
+        </md-card>
 </template>
 
 <script>
@@ -10,36 +14,55 @@
         name: "App",
 
         mounted() {
-            this.randomImage = this.getRandomImage();
+            this.getRandomImage();
         },
         data() {
             return {
                 imagesObjects: [
-                    {path: "images/angry-monkey.jpg", text: "Go Back to work now!"}, //TODO maybe add Dimensions
+                    {path: "images/mature_back_to_work.png", text: ""},
+
+
+                    {path: "images/angry-monkey.jpg", text: "Go Back to work now!"},
                     {path: "images/Black-Girl-Wat.jpg", text: "WAT are you doing here? Go to work"},
                     {path: "images/troll.jpg", text: "you didn't learn your lesson right? Go to work"},
                     {path: "images/angry-white-monkey.jpg", text: "Goooooo!"},
                     {path: "images/coffin-dance.jpg", text: "Go to work or dance with us!"},
                     {path: "images/spongebob.jpg", text: ""}
                 ],
-                randomImage: {path: "", text: ""}
+
+                selectedImageIndex: 0
+            }
+        },
+        computed: {
+            randomImage(){
+                return this.imagesObjects[this.selectedImageIndex];
             }
         },
         methods: {
+
             getRandomImage() {
-                return this.imagesObjects[Math.floor(Math.random() * this.imagesObjects.length)];
+                chrome.storage.local.get("settings", settingsItem => {
+                    let settings = settingsItem.settings;
+                    if(settings.allowFunnyGoBackImages){
+                        this.selectedImageIndex = Math.floor(Math.random() * this.imagesObjects.length);
+                    }
+
+                });
+
             }
         }
     };
 </script>
 
 <style scoped>
-    .center {
+    .md-card{
+        max-width: 40%;
         margin: auto;
-        margin-top: 3%;
-        width: 60%;
-        border: 1px solid #555755;
-        padding: 10px;
+        position: relative;
+        top: 40px;
         text-align: center;
+    }
+    .md-card-media img{
+        width: 95%;
     }
 </style>
--- src/goback/goback.js
+++ src/goback/goback.js
@@ -4,10 +4,10 @@ import Vue from 'vue'
 import App from './App'
 
 
-import VueMaterial from 'vue-material'
+import { MdCard } from 'vue-material/dist/components'
 import 'vue-material/dist/vue-material.min.css';
 import 'vue-material/dist/theme/default.css';
-Vue.use(VueMaterial);
+Vue.use(MdCard);
 
 /* eslint-disable no-new */
 new Vue({

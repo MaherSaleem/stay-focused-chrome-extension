699b8dc6df7d94c672ce8df58dd0326f3c2fd954
Maher <the-maher@live.com>
2020-07-12 21:17:38 +0300

    Increased version to 1.0.4

--- src/background.js
+++ src/background.js
@@ -64,7 +64,7 @@ chrome.runtime.onInstalled.addListener((details) => {
             console.log("installed Successfully");
             break;
         case 'update':
-            // alert(`prev version: ${previousVersion}, current version: ${currentVersion}`);
+            console.log(`prev version: ${previousVersion}, current version: ${currentVersion}`);
             handle103To104Upgrade(previousVersion, currentVersion);
             console.log("Updated Successfully");
             break;
